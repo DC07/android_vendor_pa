5cb98ec92249e0d9d1f833b39f164025a57c8e92
Devesh Negi <deveshnegi.3105.dn@gmail.com>
2014-11-19 23:54:41 +0530

    added support Android One (sprout)

--- products/pa_sprout.mk
+++ products/pa_sprout.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 ParanoidAndroid Project
+# Copyright (C) 2014 ParanoidAndroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +14,12 @@
 
 # Check for target product
 
-ifeq (pa_togari,$(TARGET_PRODUCT))
+ifeq (pa_sprout,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_xhdpi
 
+# Build paprefs from sources
 PREFS_FROM_SOURCE ?= false
 
 # Inherit telephony common stuff
@@ -28,17 +29,16 @@ $(call inherit-product, vendor/pa/configs/telephony.mk)
 include vendor/pa/main.mk
 
 # Inherit device configuration
-$(call inherit-product, device/sony/togari/full_togari.mk)
+$(call inherit-product, device/google/sprout/full_sprout.mk)
 
 # Override AOSP build properties
-PRODUCT_NAME := pa_togari
-PRODUCT_DEVICE := togari
-PRODUCT_BRAND := sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia Z Ultra
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=C6833 \
-    BUILD_FINGERPRINT=Sony/C6833_1274-8613/C6833:4.2.2/14.1.B.1.526/8bl_jw:user/release-keys \
-    PRIVATE_BUILD_DESC="C6833-user 4.2.2 14.1.B.1.526 8bl_jw test-keys"
+PRODUCT_NAME := pa_sprout
+PRODUCT_DEVICE := sprout
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := A1
+PRODUCT_MANUFACTURER := Google
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sprout TARGET_DEVICE=sprout
 
 endif
